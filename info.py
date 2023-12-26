9148396079fa3df6f088457ad6d2f3b3e9ebf04a
monitg <142651228+monitg@users.noreply.github.com>
2023-12-26 15:39:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,10 +24,10 @@ BOT_TOKEN = environ.get('BOT_TOKEN','')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/61ef9818986cef9554017.jpg https://telegra.ph/file/4696ff67a5bae3ea92c14.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/61ef9818986cef9554017.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/61ef9818986cef9554017.jpg")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/61ef9818986cef9554017.jpg")
+PICS = (environ.get('PICS', 'https://telegra.ph/file/9acca333738f41eba89f8.jpg')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/9acca333738f41eba89f8.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/9acca333738f41eba89f8.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/9acca333738f41eba89f8.jpg")
 
 # Admins, Channels & User
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
